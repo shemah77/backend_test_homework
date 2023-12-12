6baa55cc83bbc32ef68c2d0df9c8f097790a5ac0
petrovvv <shemahanov@gmail.com>
2023-12-12 12:37:29 +0300

    nee file sprint 1 added

---
+++ practicum.py
@@ -0,0 +1,8 @@
+# Аннотируем параметр функции: "значение name должно быть типа str!"
+
+def we_crash_all(name: str):
+    return 'Привет, ' + name + ', мы всё сломали!'
+print(we_crash_all('Наташа '))
+
+
+
