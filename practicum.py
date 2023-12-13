6a4415cb6b77901cd09e190cf2be97b352a6c9a5
Andrey Shem <shemah@yandex.ru>
2023-12-13 09:04:19 +0300

    update practicum2

--- practicum.py
+++ practicum.py
@@ -1,8 +1,25 @@
 # Аннотируем параметр функции: "значение name должно быть типа str!"
+from math import *
 
-def we_crash_all(name: str):
-    return 'Привет, ' + name + ', мы всё сломали!'
-print(we_crash_all('Наташа '))
+def add_numbers(x, y):
+    return x + y
+
+
+def  CalculateSquareRoot (Number:str ):
+     return  sqrt(Number)
+
+def calc(your_number) :
+    if your_number<=0:
+        return    
+     
+    root = 0
+    return f"Мы вычислили квадратный корень из введённого вами числа. Это будет: {CalculateSquareRoot(your_number)}"
 
 
 
+x = 10
+y = 5
+
+print('Сумма чисел: ', add_numbers(x, y))
+
+print(calc (25.5))
