979c84c38135cd11f0663df01cf18207dc3b7a47
petrovvv <shemahanov@gmail.com>
2023-12-18 16:47:41 +0300

    new update phone class

--- program.py
+++ program.py
@@ -1,16 +1,38 @@
+class Phone:
+    # Атрибут класса.
+    line_type = 'проводной'
+
+    def __init__(self, dial_type_value):
+        # Атрибут объекта.
+        self.dial_type = dial_type_value
 
 
-class Employee():
-    pass
+# Создать объект класса Phone.
+rotary_phone = Phone(dial_type_value='дисковый')
+keypad_phone = Phone(dial_type_value='кнопочный')
 
+# Распечатать значение атрибута класса.
+print(f'Тип линии: {rotary_phone.line_type}')
+print(f'Тип линии: {keypad_phone.line_type}')
 
-developer = Employee()
-manager = Employee()
+# Поменять значение атрибута line_type для объекта rotary_phone.
+rotary_phone.line_type = 'радио'
 
+# Снова распечатать значения.
+print(f'Тип линии: {rotary_phone.line_type}')
+print(f'Тип линии: {keypad_phone.line_type}')
 
+# Поменять значение атрибута класса через класс.
+Phone.line_type = 'спутниковый'
 
-# Создать экземпляр класса Phone().
-my_phone = Phone()
+# Снова распечатать значения.
+print(f'Тип линии: {rotary_phone.line_type}')
+print(f'Тип линии: {keypad_phone.line_type}')
 
-print(type(my_phone))
-
+# Выведется:
+# Тип линии: проводной
+# Тип линии: проводной
+# Тип линии: радио
+# Тип линии: проводной
+# Тип линии: радио
+# Тип линии: спутниковый
