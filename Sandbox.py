7f80cf1b37ce7763886d689e2a2256ba3d10cd51
Andrey Shem <shemah@yandex.ru>
2023-12-19 00:53:03 +0300

    New lesson знаком ООП

--- Sandbox.py
+++ Sandbox.py
@@ -16,13 +16,25 @@ class MobilePhone(Phone):
     # Переопределить значение атрибута line_type класса Phone.
     line_type = 'беспроводной'
 
+    battery_type = 'Li-ion'
+
+    # Конструктор класса MobilePhone с новым параметром — network_type.
+    def __init__(self, dial_type_value, network_type):
+        # Новый атрибут объекта.
+        self.network_type = network_type
+        # Вызов родительского конструктора.
+        super().__init__(dial_type_value)
+
+    def start_game(self):
+        print('Игра запущена!')
+
     # Переопределить метод ring() класса Phone.
     def ring(self):
         print('Дзынь-дзынь!')
 
 
 rotary_phone = Phone('дисковый')
-mobile_phone = MobilePhone('сенсорный')
+mobile_phone = MobilePhone('сенсорный', 'LTE')
 
 # Распечатать значение атрибута line_type для объекта класса Phone.
 print(rotary_phone.line_type)
@@ -33,6 +45,8 @@ rotary_phone.ring()
 print(mobile_phone.line_type)
 # Вызвать метод ring() для объекта класса MobilePhone.
 mobile_phone.ring()
+print(mobile_phone.network_type)
+mobile_phone.start_game()
 
 # Вывод:
 # проводной
--- practicum.py
+++ practicum.py
@@ -1,24 +1,41 @@
 class Employee:
-    # Вместо инструкции pass напишите свой код.
     vacation_days = 28
 
-    def __init__(self, gender, first_name, second_name):
-        self.gender = gender
+    def __init__(self, first_name, second_name, gender):
         self.first_name = first_name
         self.second_name = second_name
-        self.remaining_vacation_days = Employee.vacation_days
+        self.gender = gender
+        self.remaining_vacation_days = self.vacation_days
 
-
-    def consume_vacation(self, consumed_days:int):
-        self.remaining_vacation_days -= consumed_days
+    def consume_vacation(self, days):
+        self.remaining_vacation_days -= days
 
     def get_vacation_details(self):
-        return f'Остаток отпускных дней: {self.remaining_vacation_days}'
+        return f'Остаток отпускных дней: {self.remaining_vacation_days}.'
+
+
+class FullTimeEmployee(Employee):
+    vacation_days = 28
+    def get_unpaid_vacation(self, unpaid_vacation: str, unpaid_days: int):
+        return (f'Начало неоплачиваемого отпуска: {unpaid_vacation}, '
+                f'продолжительность: {unpaid_days} дней.'
+                )
+
+    def __init__(self, first_name, second_name, gender):
+        self.remaining_vacation_days = FullTimeEmployee.vacation_days
+        super().__init__(first_name, second_name, gender)
+
+
+class PartTimeEmployee(Employee):
+    vacation_days = 14
+
+    def __init__(self, first_name, second_name, gender):
+        self.remaining_vacation_days = PartTimeEmployee.vacation_days
+        super().__init__(first_name, second_name, gender)
 
 
 
 
-    pass
 
 # Создайте экземпляры класса Employee с различными значениями атрибутов.
 employee1 = Employee(gender='м', first_name='Rob', second_name='Cruso')
@@ -27,17 +44,28 @@ employee2 = Employee(gender='ж', first_name='Ira', second_name='Crusovkina')
 
 
 # Допишите код для вывода информации о сотрудниках.
-print(f' Имя: {employee1.first_name},'
-      f' Фамилия: {employee1.second_name},'
-      f' Пол: {employee1.gender}, '
-      f'Отпускных дней в году: {employee1.vacation_days}.')
+# print(f' Имя: {employee1.first_name},'
+#       f' Фамилия: {employee1.second_name},'
+#       f' Пол: {employee1.gender}, '
+#       f'Отпускных дней в году: {employee1.vacation_days}.')
+#
+# print(f' Имя: {employee2.first_name},'
+#       f' Фамилия: {employee2.second_name},'
+#       f' Пол: {employee2.gender}, '
+#       f'Отпускных дней в году: {employee2.vacation_days}.')
+#
+# employee1.consume_vacation(7)
+# print (employee1.get_vacation_details())
 
-print(f' Имя: {employee2.first_name},'
-      f' Фамилия: {employee2.second_name},'
-      f' Пол: {employee2.gender}, '
-      f'Отпускных дней в году: {employee2.vacation_days}.')
-
-employee1.consume_vacation(7)
-print (employee1.get_vacation_details())
+# Пример использования:
+full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
 
 
+# print(part_time_employee.vacation_days)
+
+# print(full_time_employee.vacation_days)
+
+
+print(part_time_employee.get_vacation_details())
