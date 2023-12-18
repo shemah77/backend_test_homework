9917056024a9657990747dae6cbf7c76e670e986
Andrey Shem <shemah@yandex.ru>
2023-12-18 21:11:20 +0300

    Class Employee

--- practicum.py
+++ practicum.py
@@ -1,15 +1,31 @@
-class Phone:
-    line_type = 'проводной'
+class Employee:
+    # Вместо инструкции pass напишите свой код.
+    vacation_days = 28
+
+    def __init__(self, gender, first_name, second_name):
+        self.gender = gender
+        self.first_name = first_name
+        self.second_name = second_name
+
+    pass
+
+# Создайте экземпляры класса Employee с различными значениями атрибутов.
+employee1 = Employee(gender='м', first_name='Rob', second_name='Cruso')
+employee2 = Employee(gender='ж', first_name='Ira', second_name='Crusovkina')
 
 
-rotary_phone = Phone()
-keypad_phone = Phone()
 
-# Печать содержимого атрибута line_type через объект rotary_phone.
-print(rotary_phone.line_type)
-# Печать содержимого атрибута line_type через объект keypad_phone.
-print(keypad_phone.line_type)
+# Допишите код для вывода информации о сотрудниках.
+print(f' Имя: {employee1.first_name},'
+      f' Фамилия: {employee1.second_name},'
+      f' Пол: {employee1.gender}, '
+      f'Отпускных дней в году: {employee1.vacation_days}.')
 
-# Выведется:
-# проводной
-# проводной
+print(f' Имя: {employee2.first_name},'
+      f' Фамилия: {employee2.second_name},'
+      f' Пол: {employee2.gender}, '
+      f'Отпускных дней в году: {employee2.vacation_days}.')
+
+
+
+
