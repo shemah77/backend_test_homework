304693f3360dab8904b692d91c0b372c8de2f19d
Andrey Shem <shemah@yandex.ru>
2024-01-26 14:17:40 +0300

    Sprint 4 review update

--- Sprint 4 Final.py
+++ Sprint 4 Final.py
@@ -1,12 +1,18 @@
-def main():
-    robot_weight_array: list = [int(x) for x in input().split()]  # массив роботов
-    platform_limit: int = int(input())  # ограничение веса платформы
+# id задачи в Яндекс Контест 105875402
+def input_data():
+    '''input data from user'''
+    robot_weight_array: list = [int(x) for x in input().split()]
+    platform_limit: int = int(input())
+    return robot_weight_array, platform_limit
+
+
+def platform(robot_weight_array: list, platform_limit:int):
+    '''This function for robot weight calculation and platform'''
+
     number_of_platform: int = 0
     left: int = 0
     right: int = len(robot_weight_array)-1
-
     robot_weight_array.sort()
-
     while left <= right:
         if robot_weight_array[right] == platform_limit:
             number_of_platform += 1
@@ -25,7 +31,15 @@ def main():
                 number_of_platform += 2
             left += 1
             right -= 1
-    print(number_of_platform)
+
+    return number_of_platform
+
+
+def main ():
+    '''главная функция '''
+    robot_weight_array, platform_limit = input_data()
+    print(platform(robot_weight_array, platform_limit))
+
 
 
 if __name__ == '__main__':
