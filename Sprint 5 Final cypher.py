7710b04c3012cb254772b08ab8d8059a1baf972b
Andrey Shem <shemah@yandex.ru>
2024-01-30 20:54:18 +0300

    Sprint 5 Done

--- Sprint 5 Final cypher.py
+++ Sprint 5 Final cypher.py
@@ -6,17 +6,10 @@ globals()
 DIGIT = '0123456789'
 CHAR = 'abcdifghijklmnopqrstuvwxyz'
 
-
-
 def decode_instructions(instructions: str) -> str:
     stack: list = []
-    # stack_dig = []
-    # stack_char = []
-    # fin = ''
-    symb = ''
-    fin = ''
     i: int = 0
-    num1: int= 0
+    num1: int = 0
     while i < len(instructions):
         if instructions[i] in DIGIT:
             # num = DIGIT.index(instructions[i])
@@ -27,29 +20,27 @@ def decode_instructions(instructions: str) -> str:
             num1: int = 0
 
         elif instructions[i] == ']':
-            symb = ''
-            fin = ''
+            symb: str = ''
+            fin: str = ''
 
             while stack and stack[-1] != '[':
                 # print (stack[-1])
                 if stack[-1].isalpha():
-                    symb  = stack.pop() + symb
+                    symb = stack.pop() + symb
 
             stack.pop()
 
             if str(stack[-1]).isdigit():
                 num = stack.pop()
-                for n in range(1, int(num) + 1): fin = symb  + fin
+                for n in range(1, int(num) + 1): fin = symb + fin
 
             stack.append(fin)
 
             i += 1
         else:
-            char = instructions[i]
-            stack.append(char)
+            stack.append(instructions[i])
             i += 1
 
-    # return ''.join(stack)
     return ''.join(stack)
 
 
@@ -60,9 +51,9 @@ def main ():
     # a_level = parse(cypher_str)
     b_level = []
     # print(a_level)
-    abc = decode_instructions(cypher_str)
-
-    print(abc)
+    res: str
+    res = decode_instructions(cypher_str)
+    print(res)
 
 if __name__ == '__main__':
     main()
