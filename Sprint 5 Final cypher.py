badcbfc4f15bf93087d2b46df51c2bc2d30eba5b
Andrey Shem <shemah@yandex.ru>
2024-02-04 10:37:21 +0300

    Sprint 5 Final review check

--- Sprint 5 Final cypher.py
+++ Sprint 5 Final cypher.py
@@ -1,59 +1,57 @@
 
 # Яндекс контест № задачи 106243150
 
-# DIGIT = '0123456789'
-# CHAR = 'abcdifghijklmnopqrstuvwxyz'
 import string
 
-DIGIT = string.digits
-CHAR = string.ascii_letters
+DIGIT: str = string.digits
+CHAR: str = string.ascii_letters
+CONST_RAZR: int = 10
 
 
-def decode_instructions(instructions: str) -> str:
+def decode_instructions(instructions: str) -> list:
     stack: list = []
-    i: int = 0
+    i_count: int = 0
     num1: int = 0
-    while i < len(instructions):
-        if instructions[i] in DIGIT:
+    while i_count < len(instructions):
+        if instructions[i_count] in DIGIT:
 
-            while i < len(instructions) and instructions[i].isdigit():
-                num1 = num1 * 10 + int(instructions[i])
-                i += 1
+            while i_count < len(instructions) and instructions[i_count].isdigit():
+                num1 = num1 * CONST_RAZR + int(instructions[i_count])
+                i_count += 1
             stack.append(str(num1))
             num1: int = 0
 
-        elif instructions[i] == ']':
+        elif instructions[i_count] == ']':
             symb: str = ''
             fin: str = ''
 
             while stack and stack[-1] != '[':
-                # print (stack[-1])
+
                 if stack[-1].isalpha():
                     symb = stack.pop() + symb
 
             stack.pop()
 
             if str(stack[-1]).isdigit():
-                num = stack.pop()
-                for n in range(1, int(num) + 1): fin = symb + fin
+                num: str = stack.pop()
+                for n in range(1, int(num) + 1):
+                    fin = symb + fin
 
             stack.append(fin)
 
-            i += 1
+            i_count += 1
         else:
-            stack.append(instructions[i])
-            i += 1
+            stack.append(instructions[i_count])
+            i_count += 1
 
-    return ''.join(stack)
+    return stack
 
 
-def main ():
-    cypher_str = str(input())
-    res: str
-    res = decode_instructions(cypher_str)
-    print(res)
+def main():
+    cypher_str: str = str(input())
+    res: list = decode_instructions(cypher_str)
+    print(*res, sep='')
+
 
 if __name__ == '__main__':
     main()
-
-
--- Sprint 5 Сортировка слиянием блоков.py
+++ Sprint 5 Сортировка слиянием блоков.py
@@ -20,32 +20,84 @@ def sort_by_bloks(sort_number:int, sort_list:list) -> int:
     else:
         max_in_block = max(sort_list[0:idx])
 
-    if max_in_block - 1 > len (sort_list[0:idx]):
-        if sort_list == [2, 7, 0, 3, 6, 4, 1, 5, 8, 9]:
-            bloks = 3
-            return bloks
-        bloks = 1
-        return bloks
-    else:
-        max_in_block = sort_list[0]
-
-        for i in range(len(sort_list)):
-            mega_block.append(sort_list[i])
-            max_in_block = max(mega_block)
-            if (sort_list[i] - max_in_block) == 1:
+    max_in_block = sort_list[0]
+    for i in range(len(sort_list)):
+        mega_block.append(sort_list[i])
+        max_in_block = max(mega_block)
+        if (sort_list[i] - max_in_block) == 1:
+            bloks += 1
+            max_in_block = sort_list[i]
+        else:
+            a = sum(mega_block)
+            b = b + int(sort_list.index(sort_list[i]))
+            if a == b:
                 bloks += 1
-                max_in_block = sort_list[i]
-            else:
+                max_in_block = max(mega_block)
+                mega_block = []
+                b = 0
 
-                a = sum(mega_block)
-                b = b + int(sort_list.index(sort_list[i]))
+    # if max_in_block - 1 > len (sort_list[0:idx]):
+    #     max_in_block = sort_list[0]
+    #     for i in range(len(sort_list)):
+    #         mega_block.append(sort_list[i])
+    #         max_in_block = max(mega_block)
+    #         if (sort_list[i] - max_in_block) == 1:
+    #             bloks += 1
+    #             max_in_block = sort_list[i]
+    #         else:
+    #             a = sum(mega_block)
+    #             b = b + int(sort_list.index(sort_list[i]))
+    #             if a == b:
+    #                 bloks += 1
+    #                 max_in_block = max(mega_block)
+    #                 mega_block = []
+    #                 b = 0
+    #     return bloks
+    # else:
+    #     max_in_block = sort_list[0]
+    #     for i in range(len(sort_list)):
+    #         mega_block.append(sort_list[i])
+    #         max_in_block = max(mega_block)
+    #         if (sort_list[i] - max_in_block) == 1:
+    #             bloks += 1
+    #             max_in_block = sort_list[i]
+    #         else:
+    #             a = sum(mega_block)
+    #             b = b + int(sort_list.index(sort_list[i]))
+    #             if a == b:
+    #                 bloks += 1
+    #                 max_in_block = max(mega_block)
+    #                 mega_block = []
+    #                 b = 0
+    #
+    #
 
-                if a == b :
-                    bloks += 1
-                    max_in_block = max(mega_block)
-                    mega_block = []
-                    b = 0
 
+
+
+    # if max_in_block - 1 > len (sort_list[0:idx]):
+    #     if sort_list == [2, 7, 0, 3, 6, 4, 1, 5, 8, 9]:
+    #         bloks = 3
+    #         return bloks
+    #     bloks = 1
+    #     return bloks
+    # else:
+    # max_in_block = sort_list[0]
+    #
+    # for i in range(len(sort_list)):
+    #     mega_block.append(sort_list[i])
+    #     max_in_block = max(mega_block)
+    #     if (sort_list[i] - max_in_block) == 1:
+    #         bloks += 1
+    #         max_in_block = sort_list[i]
+    #     else:
+    #         a = sum(mega_block)
+    #         b = b + int(sort_list.index(sort_list[i])
+    #         if a == b:
+    #             bloks += 1
+    #             max_in_block = max(mega_block)
+    #             mega_block = []
+    #             b = 0
 
     return bloks
 
